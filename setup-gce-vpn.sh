9423a34f11aafefac055cc5a0d218c0ba0c7d9d4
Ruijian Cao <cao7113@hotmail.com>
2018-09-05 11:33:40 +0800

    make algo server configable

--- setup-gce-vpn.sh
+++ setup-gce-vpn.sh
@@ -3,6 +3,8 @@ set -ex
 # ref https://github.com/trailofbits/algo#deploy-the-algo-server
 # run this on a working station(recommend ubuntu18.04)
 # trigger installation process on cloud provider, eg. gce
+algo_server=${1:-algo}
+echo server: $algo_server
 
 # python 2 required
 sudo apt install -y python
@@ -33,10 +35,10 @@ python -m virtualenv --python=`which python2` env &&
     python -m pip install -U pip &&
     python -m pip install -r requirements.txt
 
-# install way1
+## install way1
 # ./algo
 
-# install way2
+## install way2
 # source env/bin/activate && ansible-playbook main.yml $@
 ## update users
 # source env/bin/activate && ansible-playbook users.yml -t update-users $@
@@ -46,22 +48,12 @@ python -m virtualenv --python=`which python2` env &&
 # ansible-playbook --verbose --tags=a,b --extra-vars='name=geek age=3'
 # set additional variables as key=value or YAML/JSON, if filename prepend with @
 
-#ansible-playbook main.yml -e "provider=digitalocean
-        #server_name=algo
-        #ondemand_cellular=false
-        #ondemand_wifi=false
-        #local_dns=true
-        #ssh_tunneling=true
-        #windows=false
-        #store_cakey=true
-        #region=ams3
-        #do_token=token"
-
 ## google gce
 ansible-playbook main.yml -vvv -e "provider=gce
-  server_name=algo
-  ondemand_cellular=false
+  server_name=${algo_server}
+  ondemand_cellular=true
   ondemand_wifi=true
+  ondemand_wifi_exclude=test
   local_dns=true
   ssh_tunneling=true
   windows=false
@@ -75,5 +67,5 @@ ansible-playbook main.yml -vvv -e "provider=gce
 # - gcp region
 # - gce credential file
 
-echo ==congrats to algo vpn!
+echo ==congrats to setup ${algo_server} vpn!
 echo ==save your configs at your hand!
